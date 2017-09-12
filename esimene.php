3488c6b76271cadaddcc1e22294b35d0cd245a02
Liisa Jakovleva <liisajako@users.noreply.github.com>
2017-09-12 14:20:45 +0300

    lisatud esimene php

---
+++ esimene.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Liisa Jakovleva veebiprogremise asjad</title>
+
+</head>
+<body>
+	<h1>Liisa Jakovleva</h1>
+	<p>See veebileht on loodud veebiprogrammeerimise kursusel ning ei sisalda mingisugust tõsiseltvõetavat sisu.</p>
+	
+	<?php
+		echo "See on esimene jupp PHP abil väljastatud teksti!";
+	?>
+
+</body>
+</html>
